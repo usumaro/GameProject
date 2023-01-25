7cb970965cb42000d390608c0d803b5537d8df49
usumaro <usuviich@gmail.com>
2023-01-25 13:29:24 +0900

    SQL修正

--- selecttest4.php
+++ selecttest4.php
@@ -4,9 +4,16 @@ require_once('mysql_connect2.php');
   $db = connectDB();
 
 $sql = "DELETE FROM position_table ";
-$sql2 =" ALTER TABLE position_table auto_increment = 1 ";
+
   
-  $result = mysqli_query($db, $sql,$sql2);
+  $result = mysqli_query($db, $sql);
+
+
+
+  $sql =" ALTER TABLE position_table auto_increment = 1 ";
+
+
+  $result = mysqli_query($db, $sql);
 
 
   $db = mysqli_close($db);
--- selecttest5.php
+++ selecttest5.php
@@ -3,10 +3,14 @@
 require_once('mysql_connect2.php');
   $db = connectDB();
 
-$sql = " DELETE FROM  bestposition_table  ";
-$sql2 = "INSERT INTO bestposition_table SELECT * FROM position_table";
+ $sql = " DELETE FROM  bestposition_table  ";
+
   
-  mysqli_query($db, $sql,$sql2);
+  mysqli_query($db, $sql);
+
+  $sql = "INSERT INTO bestposition_table SELECT * FROM position_table";
+
+  mysqli_query($db, $sql);
 
 
   $db = mysqli_close($db);
