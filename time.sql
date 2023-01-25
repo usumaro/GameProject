817ec1cad162aeda5204b7c6464b9896c697e287
usumaro <usuviich@gmail.com>
2023-01-25 13:38:11 +0900

    SQL更新

--- time.sql
+++ time.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2022-12-20 13:56:44
+-- 生成日時: 2023-01-25 13:37:36
 -- サーバのバージョン： 10.4.24-MariaDB
 -- PHP のバージョン: 7.4.29
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `bestposition_table`
+--
+
+CREATE TABLE `bestposition_table` (
+  `id` int(11) NOT NULL,
+  `xb` text NOT NULL,
+  `yb` text NOT NULL,
+  `zb` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `position_table`
+--
+
+CREATE TABLE `position_table` (
+  `id` int(11) NOT NULL,
+  `x` text NOT NULL,
+  `y` text NOT NULL,
+  `z` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `time_table`
 --
 
@@ -39,16 +65,16 @@ CREATE TABLE `time_table` (
 --
 
 INSERT INTO `time_table` (`time_id`, `name`, `time`, `time_date`) VALUES
-(1, 'a', 1, '0000-00-00 00:00:00'),
-(2, 'b', 5, '0000-00-00 00:00:00'),
-(3, 'c', 8, '2022-11-08 19:59:17'),
-(4, 'あいう', 15, '2022-11-17 13:11:19'),
-(5, '', 0, '2022-12-15 15:12:29'),
-(6, '', 0, '2022-12-15 15:15:21'),
 (7, '', 21.2, '2022-12-15 16:17:09'),
 (8, '', 21.2, '2022-12-15 16:17:14'),
 (9, '', 21.1, '2022-12-16 16:00:37'),
-(10, 'AAAA', 21.3, '2022-12-16 16:04:44');
+(10, 'AAAA', 21.3, '2022-12-16 16:04:44'),
+(11, 'AAAAA', 21, '2022-12-23 15:11:17'),
+(12, 'ZZ', 21.4, '2022-12-23 15:11:41'),
+(13, 'A', 21.7, '2023-01-20 16:43:19'),
+(14, 'AA', 21.2, '2023-01-24 14:11:57'),
+(15, 'AA', 21.1, '2023-01-24 14:12:39'),
+(16, 'AAA', 21.5, '2023-01-24 14:37:29');
 
 --
 -- ダンプしたテーブルのインデックス
@@ -68,7 +94,7 @@ ALTER TABLE `time_table`
 -- テーブルの AUTO_INCREMENT `time_table`
 --
 ALTER TABLE `time_table`
-  MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
